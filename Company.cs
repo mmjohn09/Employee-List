c8797f5935a7b49465501118fcd4b6a49e9495da
Maggie Johnson <mmjohn09@icloud.com>
2019-10-09 14:39:36 -0500

    completed employee list

---
+++ Company.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace classes
+{
+    public class Company
+    {
+        public string Name { get; }
+        public DateTime CreatedOn { get; }
+        public List<Employee> EmployeeList = new List<Employee>();
+        public Company(string name, DateTime companyCreatedDate)
+        {
+            Name = name;
+            CreatedOn = companyCreatedDate;
+        }
+        public void ListEmployees()
+        {
+            Console.WriteLine("Acme Employee List");
+            Console.WriteLine("-----------------");
+            foreach (Employee employee in EmployeeList)
+            {
+
+                Console.WriteLine($"{employee.FirstName} {employee.LastName}");
+            }
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime AcmeCreatedDate = new DateTime(2017, 1, 18);
+            Company Acme = new Company("Acme", AcmeCreatedDate);
+
+            Employee Maggie = new Employee()
+            {
+                FirstName = "Maggie",
+                LastName = "Johnson",
+                Title = "CEO",
+                StartDate = AcmeCreatedDate
+            };
+
+            DateTime BobStart = new DateTime(2017, 2, 3);
+            Employee Bob = new Employee()
+            {
+                FirstName = "Bob",
+                LastName = "Smith",
+                Title = "Engineer",
+                StartDate = BobStart
+            };
+
+            DateTime BillStart = new DateTime(2019, 5, 21);
+            Employee Bill = new Employee()
+            {
+                FirstName = "Bill",
+                LastName = "Jackson",
+                Title = "Accountant",
+                StartDate = BillStart
+            };
+
+            Acme.EmployeeList.Add(Maggie);
+            Acme.EmployeeList.Add(Bob);
+            Acme.EmployeeList.Add(Bill);
+
+            Acme.ListEmployees();
+            
+        }
+    }
+}
